8af6b3157ec16846cef98e5bf7659a78711e44a0
Miguel Torres <miguelmariatorresrojas@gmail.com>
2018-08-13 22:09:41 +0200

    Deduplicating @wordpress/hooks

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MyDateTimePicker from './examples/date-time-picker';
 import MyFontSizePicker from './examples/font-size-picker';
 import MyFormToggle from './examples/form-toggle';
 import MyModal from './examples/modal';
+import MyQueryControls from './examples/query-controls';
 import MyComponentWithFilters from './examples/with-filters';
 
 import '@wordpress/components/build-style/style.css';
@@ -16,6 +17,7 @@ const App = () => (
       <Route path='/font-size-picker' component={MyFontSizePicker} />
       <Route path='/form-toggle' component={MyFormToggle} />
       <Route path='/modal' component={MyModal} />
+      <Route path='/query-controls' component={MyQueryControls} />
       <Route path='/with-filters' component={MyComponentWithFilters} />
     </div>
   </Router>
